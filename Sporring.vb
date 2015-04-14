293766653351c1f79341a66b420d1725d92e11af
vetleaske <vetle.aske@gmail.com>
2015-04-14 11:13:13 +0200

    Statistikk fullført

--- Sporring.vb
+++ Sporring.vb
@@ -1107,7 +1107,6 @@ Public Class Sporring
     Public Sub eksport()
         Dim sql As String
         Dim data As New DataTable
-        Dim rad As DataRow
         Dim appXL As Excel.Application
         Dim wbXl As Excel.Workbook
         Dim shXL As Excel.Worksheet
@@ -1119,56 +1118,63 @@ Public Class Sporring
         wbXl = appXL.Workbooks.Add
         shXL = wbXl.ActiveSheet
         If Statistikk.ComboBox1.Text = "Totalpris pr salg" Then
-
+            sql = "SELECT BestillingID, Totalpris FROM `Bestilling_tilbakelevering` WHERE BestillingID NOT LIKE 0 GROUP BY BestillingID"
+            data = query(sql)
+            ' Lager oversktift til vær enklet celle.
+            shXL.Cells(1, 1).Value = "Salgs ID"
+            shXL.Cells(1, 2).Value = "Pris"
+            For i = 0 To data.Rows.Count - 1
+                'Column
+                For j = 0 To data.Columns.Count - 1
+                    shXL.Cells(i + 3, j + 1) = _
+                    data.Rows(i).Item(j)
+                Next
+            Next
         End If
         If Statistikk.ComboBox1.Text = "Antall salg per måned" Then
-
+            sql = "SELECT BestillingID, month(dato_B), year(dato_B) FROM `Bestilling_tilbakelevering` WHERE BestillingID NOT LIKE 0 GROUP BY BestillingID"
+            data = query(sql)
+            ' Lager oversktift til vær enklet celle.
+            shXL.Cells(1, 1).Value = "Salgs ID"
+            shXL.Cells(1, 2).Value = "Måned"
+            shXL.Cells(1, 3).Value = "År"
+            For i = 0 To data.Rows.Count - 1
+                'Column
+                For j = 0 To data.Columns.Count - 1
+                    shXL.Cells(i + 3, j + 1) = _
+                    data.Rows(i).Item(j)
+                Next
+            Next
         End If
-        If Statistikk.ComboBox1.Text = "Antall sykkel pr måned" Then
-
+        If Statistikk.ComboBox1.Text = "Antall sykler og utstyr pr måned" Then
+            sql = "SELECT BestillingID, COUNT(BestillingID) as Antall FROM `Bestilling_tilbakelevering`  WHERE BestillingID NOT LIKE 0 GROUP BY BestillingID"
+            data = query(sql)
+            ' Lager oversktift til vær enklet celle.
+            shXL.Cells(1, 1).Value = "Salgs ID"
+            shXL.Cells(1, 2).Value = "Antall sykler og utstyr"
+            For i = 0 To data.Rows.Count - 1
+                'Column
+                For j = 0 To data.Columns.Count - 1
+                    shXL.Cells(i + 3, j + 1) = _
+                    data.Rows(i).Item(j)
+                Next
+            Next
         End If
         If Statistikk.ComboBox1.Text = "Antall aktive kunder" Then
-
+            sql = "SELECT Utleied_av, BestillingID, DATE_FORMAT(dato_B, '%Y %m %d') FROM `Bestilling_tilbakelevering` WHERE dato_B >= last_day(now()) + interval 1 day - interval 3 month and BestillingID NOT LIKE 0 GROUP BY BestillingID"
+            data = query(sql)
+            ' Lager oversktift til vær enklet celle.
+            shXL.Cells(1, 1).Value = "Kunde"
+            shXL.Cells(1, 2).Value = "Salgs ID"
+            shXL.Cells(1, 3).Value = "År"
+            For i = 0 To data.Rows.Count - 1
+                'Column
+                For j = 0 To data.Columns.Count - 1
+                    shXL.Cells(i + 3, j + 1) = _
+                    data.Rows(i).Item(j)
+                Next
+            Next
         End If
-        ' Add table headers going cell by cell.
-        shXL.Cells(1, 1).Value = "First Name"
-        shXL.Cells(1, 2).Value = "Last Name"
-        shXL.Cells(1, 3).Value = "Full Name"
-        shXL.Cells(1, 4).Value = "Specialization"
-        ' Format A1:D1 as bold, vertical alignment = center.
-        With shXL.Range("A1", "D1")
-            .Font.Bold = True
-            .VerticalAlignment = Excel.XlVAlign.xlVAlignCenter
-        End With
-        ' Create an array to set multiple values at once.
-        Dim students(5, 2) As String
-        students(0, 0) = "Zara"
-        students(0, 1) = "Ali"
-        students(1, 0) = "Nuha"
-        students(1, 1) = "Ali"
-        students(2, 0) = "Arilia"
-        students(2, 1) = "RamKumar"
-        students(3, 0) = "Rita"
-        students(3, 1) = "Jones"
-        students(4, 0) = "Umme"
-        students(4, 1) = "Ayman"
-        ' Fill A2:B6 with an array of values (First and Last Names).
-        shXL.Range("A2", "B6").Value = students
-        ' Fill C2:C6 with a relative formula (=A2 & " " & B2).
-        raXL = shXL.Range("C2", "C6")
-        raXL.Formula = "=A2 & "" "" & B2"
-        ' Fill D2:D6 values.
-        With shXL
-            .Cells(2, 4).Value = "Biology"
-            .Cells(3, 4).Value = "Mathmematics"
-            .Cells(4, 4).Value = "Physics"
-            .Cells(5, 4).Value = "Mathmematics"
-            .Cells(6, 4).Value = "Arabic"
-        End With
-        ' AutoFit columns A:D.
-        raXL = shXL.Range("A1", "D1")
-        raXL.EntireColumn.AutoFit()
-
         ' Make sure Excel is visible and give the user control
         ' of Excel's lifetime.
         appXL.Visible = True
